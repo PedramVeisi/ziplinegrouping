5d732502d34245c419ffe2fcbf08f836e0d9c69b
Pedram Veisi <pedramveisi@gmail.com>
2025-06-18 19:11:23 -0600

    Fix typos

--- lib/matcher/email_matcher.rb
+++ lib/matcher/email_matcher.rb
@@ -4,17 +4,17 @@ require_relative 'base_matcher'
 class EmailMatcher < BaseMatcher
   def build_groups
     uf = UnionFind.new(@records.size)
-    email_to_indicies = Hash.new { |hash, key| hash[key] = [] }
+    email_to_indexes = Hash.new { |hash, key| hash[key] = [] }
 
     @records.each_with_index do |record, index|
       record.normalize_emails.each do |email|
-        email_to_indicies[email] << index
+        email_to_indexes[email] << index
       end
     end
 
-    email_to_indicies.each_value do |indices|
-      first = indices.first
-      indices.each { |index| uf.union(first, index) }
+    email_to_indexes.each_value do |indexes|
+      first = indexes.first
+      indexes.each { |index| uf.union(first, index) }
     end
 
     build_index_to_group_map(uf)
--- lib/matcher/phone_matcher.rb
+++ lib/matcher/phone_matcher.rb
@@ -4,17 +4,17 @@ require_relative 'base_matcher'
 class PhoneMatcher < BaseMatcher
   def build_groups
     uf = UnionFind.new(@records.size)
-    phone_to_indicies = Hash.new { |hash, key| hash[key] = [] }
+    phone_to_indexes = Hash.new { |hash, key| hash[key] = [] }
 
     @records.each_with_index do |record, index|
       record.normalize_phone_numbers.each do |phone|
-        phone_to_indicies[phone] << index
+        phone_to_indexes[phone] << index
       end
     end
 
-    phone_to_indicies.each_value do |indices|
-      first = indices.first
-      indices.each { |index| uf.union(first, index) }
+    phone_to_indexes.each_value do |indexes|
+      first = indexes.first
+      indexes.each { |index| uf.union(first, index) }
     end
 
     build_index_to_group_map(uf)
